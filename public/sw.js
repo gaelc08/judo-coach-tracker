a89296777f9c9e76d0836c953405b098f2b863ad
gaelc08 <gael.cantarero@gmail.com>
2026-03-15 18:19:15 +0100

    Rename export buttons and bump app version

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'judo-coach-pwa-v37';
-const APP_BUILD_ID = '2026-03-15-r01';
+const CACHE_VERSION = 'judo-coach-pwa-v38';
+const APP_BUILD_ID = '2026-03-15-r02';
 const BASE_PATH = new URL('./', self.location.href).pathname;
 const INDEX_URL = `${BASE_PATH}index.html`;
 const OFFLINE_URL = `${BASE_PATH}offline.html`;
