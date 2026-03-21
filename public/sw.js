0d4a1635ffba3217be210da10e78d776eaf944b9
Gaël Cantarero <gael.cantarero@gmail.com>
2026-03-21 00:12:06 +0000

    chore: bump app version after sync-helloasso fix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'judo-coach-pwa-v41';
-const APP_BUILD_ID = '2026-03-21-r01';
+const CACHE_VERSION = 'judo-coach-pwa-v42';
+const APP_BUILD_ID = '2026-03-21-r02';
 const BASE_PATH = new URL('./', self.location.href).pathname;
 const INDEX_URL = `${BASE_PATH}index.html`;
 const OFFLINE_URL = `${BASE_PATH}offline.html`;
