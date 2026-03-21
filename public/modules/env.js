ab17f95b23e07c5c4e61d7517c6f0cf2516191fa
Gaël Cantarero <gael.cantarero@gmail.com>
2026-03-21 00:10:16 +0000

    chore: bump app version after HelloAsso sync feature

--- public/modules/env.js
+++ public/modules/env.js
@@ -43,8 +43,8 @@ export const supabaseKey = effectiveEnv === 'dev'
   ? (localDevKeyOverride || DEV_SUPABASE_KEY || PROD_SUPABASE_KEY)
   : PROD_SUPABASE_KEY;
 
-export const VERSION_DATE = '2026-03-15';
-export const VERSION_INCREMENT = '04';
+export const VERSION_DATE = '2026-03-21';
+export const VERSION_INCREMENT = '01';
 export const BUILD_ID = `${VERSION_DATE}-r${VERSION_INCREMENT}`;
 
 if (effectiveEnv === 'dev' && !localDevKeyOverride) {
